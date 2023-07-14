0c564891d153eb071f448660abc836ee4c893c2e
Gameunkulus <bugr24-8@mail.ru>
2023-07-14 13:45:06 +0300

    program redacted 2

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,21 +1,22 @@
 package ru.netology.service;
 
-import org.testng.AssertJUnit;
-import org.testng.annotations.Test;
+import org.testng.Assert;
 
-import static org.testng.Assert.*;
 
 public class CashbackHackServiceTest {
 
-    @Test
+    @org.testng.annotations.Test
     public void testRemain() {
         CashbackHackService service = new CashbackHackService();
+        //проверка ввода обычного числа
         int expected1 = 990;
-        AssertJUnit.assertEquals(expected1, service.remain(10));
+        Assert.assertEquals(expected1, service.remain(10));
+        //проверка ввода отрицательного числа
         int expected2 = 1010;
-        AssertJUnit.assertEquals(expected2, service.remain(-10));
+        Assert.assertEquals(expected2, service.remain(-10));
+        //проверка ввода нулевого числа
         int expected3 = 1000;
-        AssertJUnit.assertEquals(expected3, service.remain(0));
+        Assert.assertEquals(expected3, service.remain(0));
 
     }
 }
