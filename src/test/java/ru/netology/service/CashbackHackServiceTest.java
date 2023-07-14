a4665112507f749c1ea86e9b000497b10a883bdf
Gameunkulus <bugr24-8@mail.ru>
2023-07-14 13:34:08 +0300

    program redacted 2

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -7,10 +7,13 @@ public class CashbackHackServiceTest {
     @org.junit.Test
     public void remainTest() {
         CashbackHackService service = new CashbackHackService();
+        //проверка при обычном числе
         int expected1 = 990;
         Assert.assertEquals(expected1, service.remain(10));
+        //проверка при отрицательном числе
         int expected2 = 1010;
         Assert.assertEquals(expected2, service.remain(-10));
+        //проверка при нулевом числе
         int expected3 = 1000;
         Assert.assertEquals(expected3, service.remain(0));
     }
